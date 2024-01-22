228283c85e0dd3dc8f49d3dec6fc0b6be5809fbb
Robby Awaldi <47056508+robbyawaldi@users.noreply.github.com>
2024-01-22 16:16:01 +0700

    fix prettier

--- src/__test__/openapi-generator.test.ts
+++ src/__test__/openapi-generator.test.ts
@@ -6,203 +6,199 @@ test("generator should create openapi", async () => {
   const result = await generator.generateOpenApi();
   expect(result.replace(/\s/g, "")).toBe(
     `
-    openapi: 3.0.0
-
-    info:
-      title: API Specification
-      description: API Specification
-      version: 1.0.0
-    
-    paths:/summary/overview:
-      post:
-        responses:
-          200:
-            description: "Description"
-            content:
-              application/json:
-                schema:
-                  $ref: "#/components/schemas/overview"
-        requestBody:
-          required: true
+    openapi: "3.0.0"
+info:
+  title: "API Specification"
+  description: "API Specification"
+  version: "1.0.0"
+paths:
+  /summary/overview:
+    post:
+      responses:
+        200:
+          description: "Description"
           content:
             application/json:
               schema:
-                type: "object"
-                properties:
-                  date:
-                    type: "string"
-    
-    /users/detail:
-      post:
-        responses:
-          200:
-            description: "Description"
-            content:
-              application/json:
-                schema:
-                  $ref: "#/components/schemas/detail"
-    
-    /users:
-      post:
-        responses:
-          200:
-            description: "Description"
-            content:
-              application/json:
-                schema:
-                  $ref: "#/components/schemas/users"
-        requestBody:
-          required: true
-          content:
-            application/json:
-              schema:
-                type: "object"
-                properties:
-                  date:
-                    type: "string"
-    
-    components:
-      schemas:
-        overview:
-          type: "object"
-          properties:
-            totalSalesOverview:
+                $ref: "#/components/schemas/overview"
+      requestBody:
+        required: true
+        content:
+          application/json:
+            schema:
               type: "object"
               properties:
-                totalSales:
-                  type: "number"
-                monthlyGrowth:
-                  type: "number"
-                topSellingProduct:
+                date:
                   type: "string"
-            salesByRegion:
+  /users/detail:
+    post:
+      responses:
+        200:
+          description: "Description"
+          content:
+            application/json:
+              schema:
+                $ref: "#/components/schemas/detail"
+  /users:
+    post:
+      responses:
+        200:
+          description: "Description"
+          content:
+            application/json:
+              schema:
+                $ref: "#/components/schemas/users"
+      requestBody:
+        required: true
+        content:
+          application/json:
+            schema:
               type: "object"
               properties:
-                NorthAmerica:
-                  type: "number"
-                Europe:
-                  type: "number"
-                AsiaPacific:
-                  type: "number"
-                RestOfWorld:
-                  type: "number"
-            productPerformance:
-              type: "object"
-              properties:
-                WidgetX:
-                  type: "number"
-                WidgetY:
-                  type: "number"
-                WidgetZ:
-                  type: "number"
-            salesChannels:
-              type: "object"
-              properties:
-                online:
-                  type: "number"
-                offline:
-                  type: "number"
-            customerDemographics:
-              type: "object"
-              properties:
-                newCustomers:
-                  type: "number"
-                returningCustomers:
-                  type: "number"
-                averageCustomerAge:
-                  type: "number"
-                malePercentage:
-                  type: "number"
-                femalePercentage:
-                  type: "number"
-            salesFunnel:
-              type: "object"
-              properties:
-                leads:
-                  type: "number"
-                opportunities:
-                  type: "number"
-                conversions:
-                  type: "number"
-                conversionRate:
-                  type: "number"
-            topPerformingSalesReps:
-              type: "object"
-              properties:
-                JohnDoe:
-                  type: "number"
-                JaneSmith:
-                  type: "number"
-                MarkJohnson:
-                  type: "number"
-            inventoryStatus:
-              type: "object"
-              properties:
-                totalStockValue:
-                  type: "number"
-                outOfStockItems:
-                  type: "number"
-                lowStockItems:
-                  type: "number"
-            customerSatisfaction:
-              type: "object"
-              properties:
-                netPromoterScore:
-                  type: "number"
-                customerRatings:
-                  type: "object"
-                  properties:
-                    5stars:
-                      type: "number"
-                    4stars:
-                      type: "number"
-                    3stars:
-                      type: "number"
-                    2stars:
-                      type: "number"
-                    1star:
-                      type: "number"
-            financialSummary:
-              type: "object"
-              properties:
-                grossProfitMargin:
-                  type: "number"
-                operatingExpenses:
-                  type: "number"
-                netProfit:
-                  type: "number"
-        detail:
+                date:
+                  type: "string"
+components:
+  schemas:
+    overview:
+      type: "object"
+      properties:
+        totalSalesOverview:
           type: "object"
           properties:
-            userId:
+            totalSales:
               type: "number"
-            username:
+            monthlyGrowth:
+              type: "number"
+            topSellingProduct:
               type: "string"
-            fullName:
-              type: "string"
-            email:
-              type: "string"
-            role:
-              type: "string"
-            lastLogin:
-              type: "string"
-        users:
-          type: "array"
-          items:
-            type: "object"
-            properties:
-              userId:
-                type: "number"
-              username:
-                type: "string"
-              fullName:
-                type: "string"
-              email:
-                type: "string"
-              role:
-                type: "string"
-              lastLogin:
-                type: "string"
+        salesByRegion:
+          type: "object"
+          properties:
+            NorthAmerica:
+              type: "number"
+            Europe:
+              type: "number"
+            AsiaPacific:
+              type: "number"
+            RestOfWorld:
+              type: "number"
+        productPerformance:
+          type: "object"
+          properties:
+            WidgetX:
+              type: "number"
+            WidgetY:
+              type: "number"
+            WidgetZ:
+              type: "number"
+        salesChannels:
+          type: "object"
+          properties:
+            online:
+              type: "number"
+            offline:
+              type: "number"
+        customerDemographics:
+          type: "object"
+          properties:
+            newCustomers:
+              type: "number"
+            returningCustomers:
+              type: "number"
+            averageCustomerAge:
+              type: "number"
+            malePercentage:
+              type: "number"
+            femalePercentage:
+              type: "number"
+        salesFunnel:
+          type: "object"
+          properties:
+            leads:
+              type: "number"
+            opportunities:
+              type: "number"
+            conversions:
+              type: "number"
+            conversionRate:
+              type: "number"
+        topPerformingSalesReps:
+          type: "object"
+          properties:
+            JohnDoe:
+              type: "number"
+            JaneSmith:
+              type: "number"
+            MarkJohnson:
+              type: "number"
+        inventoryStatus:
+          type: "object"
+          properties:
+            totalStockValue:
+              type: "number"
+            outOfStockItems:
+              type: "number"
+            lowStockItems:
+              type: "number"
+        customerSatisfaction:
+          type: "object"
+          properties:
+            netPromoterScore:
+              type: "number"
+            customerRatings:
+              type: "object"
+              properties:
+                5stars:
+                  type: "number"
+                4stars:
+                  type: "number"
+                3stars:
+                  type: "number"
+                2stars:
+                  type: "number"
+                1star:
+                  type: "number"
+        financialSummary:
+          type: "object"
+          properties:
+            grossProfitMargin:
+              type: "number"
+            operatingExpenses:
+              type: "number"
+            netProfit:
+              type: "number"
+    detail:
+      type: "object"
+      properties:
+        userId:
+          type: "number"
+        username:
+          type: "string"
+        fullName:
+          type: "string"
+        email:
+          type: "string"
+        role:
+          type: "string"
+        lastLogin:
+          type: "string"
+    users:
+      type: "array"
+      items:
+        type: "object"
+        properties:
+          userId:
+            type: "number"
+          username:
+            type: "string"
+          fullName:
+            type: "string"
+          email:
+            type: "string"
+          role:
+            type: "string"
+          lastLogin:
+            type: "string"
   `.replace(/\s/g, "")
   );
 });
--- src/openapi-generator.ts
+++ src/openapi-generator.ts
@@ -12,14 +12,16 @@ interface Files {
 export class OpenApiGenerator {
   private files: Files[];
   private directoryPath: string;
-  readonly header = `openapi: 3.0.0
-
-info:
-  title: API Specification
-  description: API Specification
-  version: 1.0.0
-
-paths:`;
+  readonly header = {
+    openapi: "3.0.0",
+    info: {
+      title: "API Specification",
+      description: "API Specification",
+      version: "1.0.0",
+    },
+    paths: {},
+    components: {},
+  };
   constructor(directoryPath: string) {
     this.files = this.mapFileNames(directoryPath).map((map) => ({
       name: this.createSchemaName(map),
@@ -30,11 +32,18 @@ paths:`;
   }
   async generateOpenApi() {
     let result = this.header;
-    result += "\n";
-    result += this.mappingPaths();
-    result += "\n";
-    result += await this.mappingSchemas();
-    return result;
+    result.paths = this.mappingPaths();
+    result.components = this.mappingSchemas();
+    return await prettier.format(YAML.stringify(result), {
+      semi: false,
+      singleQuote: false,
+      printWidth: 80,
+      tabWidth: 2,
+      trailingComma: "none",
+      arrowParens: "avoid",
+      endOfLine: "auto",
+      parser: "yaml",
+    });
   }
   private mapFileNames(directoryPath: string): string[] {
     let fileNames: string[] = [];
@@ -59,95 +68,79 @@ paths:`;
 
     return fileNames;
   }
-  private mappingPaths(): string {
-    return this.files
-      .map((file) => {
-        let requestBody: any = null;
-        try {
-          const requestFilePath = path.join(
-            this.directoryPath + "/request.config.json"
-          );
-          const requestFile = JSON.parse(
-            fs.readFileSync(requestFilePath, "utf-8")
-          );
-          const requestDataPath = path.join(
-            this.directoryPath,
-            requestFile[file.path]
-          );
-          const requestData = JSON.parse(
-            fs.readFileSync(requestDataPath, "utf-8")
-          );
-          requestBody = {
-            required: true,
-            content: {
-              "application/json": {
-                schema: this.jsonToOpenApiSchema(requestData),
-              },
+  private mappingPaths() {
+    return this.files.reduce((_paths, file) => {
+      let requestBody: any = null;
+      try {
+        const requestFilePath = path.join(
+          this.directoryPath + "/request.config.json"
+        );
+        const requestFile = JSON.parse(
+          fs.readFileSync(requestFilePath, "utf-8")
+        );
+        const requestDataPath = path.join(
+          this.directoryPath,
+          requestFile[file.path]
+        );
+        const requestData = JSON.parse(
+          fs.readFileSync(requestDataPath, "utf-8")
+        );
+        requestBody = {
+          required: true,
+          content: {
+            "application/json": {
+              schema: this.jsonToOpenApiSchema(requestData),
             },
-          };
-        } catch (err) {}
+          },
+        };
+      } catch (err) {}
 
-        const paths = file.path
-          .split("/")
-          .map((f) => f.replace(".json", ""))
-          .filter((f) => f !== "");
-        const method = paths.shift();
-        if (paths[paths.length - 1] === "index") {
-          paths.pop();
-        }
-        if (paths[paths.length - 1].includes("*")) {
-          paths[paths.length - 1] = paths[paths.length - 1].replace("*", "");
-        }
-        const formattedPath = paths.map((p) => `/${p}`).join("");
-        const result: any = {
-          [formattedPath]: {
-            [method ?? "post"]: {
-              responses: {
-                200: {
-                  description: "Description",
-                  content: {
-                    "application/json": {
-                      schema: {
-                        $ref: `#/components/schemas/${file.name}`,
-                      },
+      const paths = file.path
+        .split("/")
+        .map((f) => f.replace(".json", ""))
+        .filter((f) => f !== "");
+      const method = paths.shift();
+      if (paths[paths.length - 1] === "index") {
+        paths.pop();
+      }
+      if (paths[paths.length - 1].includes("*")) {
+        paths[paths.length - 1] = paths[paths.length - 1].replace("*", "");
+      }
+      const formattedPath = paths.map((p) => `/${p}`).join("");
+      const result: any = {
+        [formattedPath]: {
+          [method ?? "post"]: {
+            responses: {
+              200: {
+                description: "Description",
+                content: {
+                  "application/json": {
+                    schema: {
+                      $ref: `#/components/schemas/${file.name}`,
                     },
                   },
                 },
               },
             },
           },
-        };
-        if (requestBody) {
-          result[formattedPath][method ?? "post"]["requestBody"] = requestBody;
-        }
-        return YAML.stringify(result);
-      })
-      .join("\n");
-  }
-  private async mappingSchemas(): Promise<string> {
-    return await prettier.format(
-      YAML.stringify({
-        components: {
-          schemas: this.files.reduce((schemas, file) => {
-            const data = fs.readFileSync(file.dir, "utf-8");
-            (schemas as any)[file.name] = this.jsonToOpenApiSchema(
-              JSON.parse(data)
-            );
-            return schemas;
-          }, {}),
         },
-      }),
-      {
-        semi: false,
-        singleQuote: false,
-        printWidth: 80,
-        tabWidth: 2,
-        trailingComma: "none",
-        arrowParens: "avoid",
-        endOfLine: "auto",
-        parser: "yaml",
+      };
+      if (requestBody) {
+        result[formattedPath][method ?? "post"]["requestBody"] = requestBody;
       }
-    );
+      return { ..._paths, ...result };
+    }, {});
+  }
+  private mappingSchemas() {
+    return {
+      schemas: this.files.reduce((schemas, file) => {
+        const data = fs.readFileSync(file.dir, "utf-8");
+        (schemas as any)[file.name] = this.jsonToOpenApiSchema(
+          JSON.parse(data)
+        );
+        return schemas;
+      }, {}),
+    };
   }
   private createSchemaName(filePath: string) {
     let fileName = filePath.replace(".json", "");
